b87bbfad30ad6f69ad0414d262b5c2f8e6daaa7e
Dark System <darksystemcoder@gmail.com>
2023-08-10 15:22:10 -0400

    1.0.0

--- main.js
+++ main.js
@@ -1,2 +1,108 @@
 //bluetoothctl devices; bluetoothctl connect ${device.mac}
 const child_process = require('child_process')
+async function runComm(comm) {
+    return new Promise((resolve, reject) => {
+        child_process.exec(comm, (error, stdout, stderr) => {
+            if (error) {
+                reject(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                resolve(`${stderr}`);
+                return;
+            }
+            resolve(stdout)
+        });
+    })
+}
+/** 
+ * Get all devices that can be paired with.
+ * @function
+ *  @async
+ */
+async function getDevices() {
+    try {
+        var devices = []
+        var cmd = await runComm('bluetoothctl devices')
+        cmd.split('\n').forEach(line => {
+            var sections = line.split(' ')
+            devices.push({ 'mac': sections[1], 'name': sections.slice(2).join(' ') })
+        });
+        return devices
+    } catch (e) {
+        throw new Error('Error while getting devices, ' + e);
+    }
+}
+/** 
+ * Connect to a bluetooth enabled device.
+ * @async
+ * @function
+ * @param {string} name - The name of the device to disconnect from
+*/
+async function connect(name) {
+
+    try {
+        var devices = await getDevices();
+        devices.forEach(async (dev) => {
+            if (dev.name == name) {
+                await runComm(`bluetoothctl connect ${dev.mac}`)
+                return;
+            }
+        })
+        throw new Error('no device found');
+    } catch (e) {
+        throw new Error('Error while connecting, ' + e);
+    }
+
+}
+/** 
+ * Disconnect from a bluetooth device.
+ * @async
+ * @function
+ * @param {string} name - The name of the device to disconnect from
+ */
+async function disconnect(name) {
+    try {
+        var devices = await getDevices();
+        devices.forEach(async (dev) => {
+            if (dev.name == name) {
+                await runComm(`bluetoothctl disconnect ${dev.mac}`)
+                return;
+            }
+        })
+        throw new Error('no device found');
+    } catch (e) {
+        throw new Error('Error while disconnecting, ' + e);
+    }
+
+}
+/** 
+ * Turn on bluetooth.
+ * @async
+ * @function
+ */
+async function on() {
+    try {
+        await runComm('rfkill unblock bluetooth')
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+/** 
+ * Turn off bluetooth.
+ * @async
+ * @function
+ */
+async function off() {
+    try {
+        await runComm('rfkill block bluetooth')
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+/**
+ * @author Dark System <pdarksystem@gmail.com>
+ * @version 1.0.0
+ */
+var nodeBluez = { on, off, disconnect, connect, getDevices }
+module.exports = nodeBluez
---
+++ readme.md
@@ -0,0 +1,73 @@
+## Members
+
+<dl>
+<dt><a href="#nodeBluez">nodeBluez</a></dt>
+<dd></dd>
+</dl>
+
+## Functions
+
+<dl>
+<dt><a href="#getDevices">getDevices()</a></dt>
+<dd><p>Get all devices that can be paired with.</p>
+</dd>
+<dt><a href="#connect">connect(name)</a></dt>
+<dd><p>Connect to a bluetooth enabled device.</p>
+</dd>
+<dt><a href="#disconnect">disconnect(name)</a></dt>
+<dd><p>Disconnect from a bluetooth device.</p>
+</dd>
+<dt><a href="#on">on()</a></dt>
+<dd><p>Turn on bluetooth.</p>
+</dd>
+<dt><a href="#off">off()</a></dt>
+<dd><p>Turn off bluetooth.</p>
+</dd>
+</dl>
+
+<a name="nodeBluez"></a>
+
+## nodeBluez
+**Kind**: global variable  
+**Version**: 1.0.0  
+**Author**: Dark System <pdarksystem@gmail.com>  
+<a name="getDevices"></a>
+
+## getDevices()
+Get all devices that can be paired with.
+
+**Kind**: global function  
+<a name="connect"></a>
+
+## connect(name)
+Connect to a bluetooth enabled device.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| name | <code>string</code> | The name of the device to disconnect from |
+
+<a name="disconnect"></a>
+
+## disconnect(name)
+Disconnect from a bluetooth device.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| name | <code>string</code> | The name of the device to disconnect from |
+
+<a name="on"></a>
+
+## on()
+Turn on bluetooth.
+
+**Kind**: global function  
+<a name="off"></a>
+
+## off()
+Turn off bluetooth.
+
+**Kind**: global function  
